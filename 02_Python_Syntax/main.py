00191200282e3456e30cad911fc17e1592a92e13
soumadip-dev <soumadipmajila@gmail.com>
2026-02-19 22:08:38 +0530

    feat: add Section 1 covering introduction to programming fundamentals and Python basics

---
+++ 02_Python_Syntax/main.py
@@ -0,0 +1,12 @@
+print("Hello Soumadip")
+print("I am good.")
+print("How are you?")
+print(3)
+
+number = 2
+
+if number > 3:
+    print("The number is greater than 3.")
+else:
+    print("The number is less than or equal to 3.")
+
