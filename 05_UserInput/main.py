78c437a9583c22a22fe39a6a5a75302c4598f077
soumadip-dev <soumadipmajila@gmail.com>
2026-02-20 21:07:08 +0530

    feat: add code demonstrating how to take user input in Python

---
+++ 05_UserInput/main.py
@@ -0,0 +1,17 @@
+username = input("Enter your username: ")
+age = input("Enter your age: ")
+
+print(username)
+print(age)
+
+# By default, input() returns a string.
+# You can convert it to other data types as needed.
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Convert num1 and num2 to integers
+num1 = int(num1)
+num2 = int(num2)
+
+print("Sum of two numbers is:", num1 + num2)
